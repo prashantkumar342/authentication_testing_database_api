0d060be01bce5bbb577723acb128125a14981b1d
prashantkumar342 <tuhaniaji>
2024-07-15 05:10:20 +0530

    login & verify debug_01

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import connectDB from './db/connectdb.js'
 import HomeRoute from './routes/Home.route.js'
 import signupRoute from './routes/Signup.route.js'
 import loginRoute from './routes/Login.route.js'
-import tokenVerify from './routes/tokenVerify.route.js'
 import jwt from 'jsonwebtoken'
 import clearCookieRoute from './routes/ClearCookie.route.js'
 import { User as userModel } from './models/user.model.js'
@@ -29,15 +28,35 @@ app.use(bodyParser.json())
 app.use(('/', HomeRoute))
 app.use(('/api/user/register', signupRoute))
 app.use(('/api/user/login', loginRoute))
-app.use(('/tokenVerify', tokenVerify))
 app.use(('/clear/cookie', clearCookieRoute))
 app.get('/api/users', async (req, res) => {
   const users = await userModel.find()
   res.send(users)
 })
-const token = jwt.sign({ hi: 'hisfsdjk' }, 'ssdddddds')
-app.get('/create/cookie', (req, res) => {
-  res.status(200).cookie('kumar', token, { httpOnly: true }).send('cookiecreated')
+app.get('/tokenVerify', async (req, res) => {
+  const cookieToken = req.cookies.authToken
+  if (!cookieToken) {
+    return res.status(404).send('user not logged in ');
+  } else {
+    jwt.verify(cookieToken, process.env.JWT_SECRET_KEY, async (err, decoded) => {
+      if (err) {
+        return res.status(403).send('Invalid session');
+      } else {
+        const { email } = decoded
+        try {
+          const user = await userModel.findOne({ email })
+          if (user) {
+            res.status(200).send('logged in')
+          } else {
+            res.status(404).send('user not found')
+          }
+        } catch (error) {
+          res.status(500).send('Internal server error');
+        }
+      }
+    })
+  }
+
 })
 
 app.listen(process.env.PORT, () => {
